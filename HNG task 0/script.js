b47c2cf1bd64e23671980b3b90a51157351a9526
chinaemerem703 <jacobchinaemerem@gmail.com>
2025-10-16 20:09:43 +0100

    HNG task 0 profile card

---
+++ HNG task 0/script.js
@@ -0,0 +1,11 @@
+const userTimeElement = document.querySelector('[data-testid="test-user-time"]');
+
+function updateUserTime() {
+  const currentTime = Date.now();
+  userTimeElement.textContent = `Current Time: ${currentTime}ms`;
+}
+
+updateUserTime();
+setInterval(updateUserTime, 1000);
+
+
